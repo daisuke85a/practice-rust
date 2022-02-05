957c9daf9d1fbdd51a2fb9a74f94d46e9b371d40
daisuke <dsaito85a@gmail.com>
2022-02-05 13:48:09 +0900

    feat: learning text

---
+++ variable/text.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let character_1: char = 'S';
+    let character_2: char = 'f';
+
+    let smiley_face = '😃';
+
+    let string_1 = "miley ";
+
+    let string_2: &str = "ace";
+
+    println!("{} is a {}{}{}{}.", smiley_face, character_1, string_1, character_2, string_2)
+}
