55ca6f56d04059e99a99c2ee904fdbc640f3caac
daisuke <dsaito85a@gmail.com>
2022-04-09 18:04:22 +0900

    feat: 浮動小数点型

--- the-rust-programming-language/ch03-01-variables-and-mutability/variables/src/main.rs
+++ the-rust-programming-language/ch03-01-variables-and-mutability/variables/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("The value of x is :{}", x);
+    let x = 2.22222;
+    let y: f32 = 3.333333;
+    println!("The value of x is :{:.3}, y is :{:.3}", x, y);
 }
