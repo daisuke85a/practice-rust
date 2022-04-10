20df98b297982ec2d82853ce7ae47f36e8c0a7c7
daisuke <dsaito85a@gmail.com>
2022-04-10 22:12:25 +0900

    feat: 引数の所有権を返す

--- the-rust-programming-language/ch04-01-what-is-ownership/ownership/src/main.rs
+++ the-rust-programming-language/ch04-01-what-is-ownership/ownership/src/main.rs
@@ -1,15 +1,12 @@
 fn main() {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let (s2, len) = caluculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len)
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn caluculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
